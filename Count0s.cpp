f9739507189bf7a3946480c54495b3dfef86556e
1AryanMishra <86364574+1AryanMishra@users.noreply.github.com>
2021-06-28 09:20:53 +0530

    Create Count0s.cpp
    Given an array of size N consisting of only 0's and 1's. The array is sorted in such a manner that all the 1's are placed first and then they are followed by all the 0's. Find the count of all the 0's.

---
+++ Count0s.cpp
@@ -0,0 +1,26 @@
+class Solution{   
+public:
+
+
+    int countZeroes(int arr[], int n) {
+        // code here
+        int s = 0;
+        int e = n-1;
+        int f = 0;
+        while(s<=e){
+            if(arr[(s+e)/2] > 0){
+                s = ((s+e)/2)+1;
+            }
+            else if(arr[(s+e)/2] == 0 && arr[((s+e)/2)-1] == 1){
+                f = (s+e)/2;
+                break;
+            }
+            else{
+                e = ((s+e)/2)-1;
+            }
+        }
+        return n-f;
+        
+    }
+    
+};

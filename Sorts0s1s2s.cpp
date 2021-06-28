42a0ab5b37b7fb788730d23ad485fd9d62cbe7c6
1AryanMishra <86364574+1AryanMishra@users.noreply.github.com>
2021-06-28 09:28:06 +0530

    Create Sorts0s1s2s.cpp
    Given an array of size N containing only 0s, 1s, and 2s; sort the array in ascending order.

---
+++ Sorts0s1s2s.cpp
@@ -0,0 +1,33 @@
+class Solution
+{
+    public:
+    void sort012(int arr[], int n)
+    {
+        // coode here 
+        int z = 0, o = 0, i = 0;
+
+        while(i < n){
+            if(arr[i] == 0){
+                swap(arr[i], arr[z]);
+                z++;
+                if(arr[i] == 1){
+                    if(o < z){
+                        o = z;
+                    }
+                    swap(arr[i], arr[o]);
+                    o++;
+                }
+            }
+    
+            else if(arr[i] == 1){
+                if(o < z){
+                    o = z;
+                }
+                swap(arr[i], arr[o]);
+                o++;
+            }
+            i++;
+        }
+    }
+    
+};
